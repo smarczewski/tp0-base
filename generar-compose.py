85ad012cacaf3dfbe0e94cc19d079cc220fa66ad
smarczewski <smarczewski@fi.uba.ar>
2026-03-23 22:22:41 -0300

    Implementa generador de definición de Docker Compose

---
+++ generar-compose.py
@@ -0,0 +1,98 @@
+import yaml
+import argparse
+
+
+class Server(yaml.YAMLObject):
+    yaml_tag = "!Server"
+
+    def __init__(self):
+        self.container_name = "server"
+        self.image = "server:latest"
+        self.entrypoint = "python3 /main.py"
+        self.environment = ["PYTHONUNBUFFERED=1", "LOGGING_LEVEL=DEBUG"]
+        self.networks = ["testing_net"]
+
+    def __repr__(self):
+        return (
+            "%s(container_name=%r, image=%r, entrypoint=%r, environment=%r, networks=%r)"
+            % (
+                self.__class__.__name__,
+                self.container_name,
+                self.image,
+                self.entrypoint,
+                self.environment,
+                self.networks,
+            )
+        )
+
+
+class Client(yaml.YAMLObject):
+    yaml_tag = "!Client"
+
+    def __init__(self, number):
+        self.container_name = f"client{number}"
+        self.image = "client:latest"
+        self.entrypoint = "/client"
+        self.environment = [f"CLI_ID={number}", "CLI_LOG_LEVEL=DEBUG"]
+        self.networks = ["testing_net"]
+        self.depends_on = ["server"]
+
+    def __repr__(self):
+        return (
+            "%s(container_name=%r, image=%r, entrypoint=%r, environment=%r, networks=%r, depends_on=%r)"
+            % (
+                self.__class__.__name__,
+                self.container_name,
+                self.image,
+                self.entrypoint,
+                self.environment,
+                self.networks,
+                self.depends_on,
+            )
+        )
+
+
+class TestingNet(yaml.YAMLObject):
+    yaml_tag = "!TestingNet"
+
+    def __init__(self):
+        self.ipam = {"driver": "default", "config": [{"subnet": "172.25.125.0/24"}]}
+
+    def __repr__(self):
+        return "%s(ipam=%r)" % (
+            self.__class__.__name__,
+            self.ipam,
+        )
+
+
+def generar_data(n_clientes):
+    data = {"name": "tp0", "services": {}, "networks": {}}
+
+    data["services"]["server"] = Server()
+
+    for n in range(1, n_clientes + 1):
+        data["services"][f"client{n}"] = Client(number=n)
+
+    data["networks"]["testing_net"] = TestingNet()
+
+    return data
+
+
+def main():
+    argumentos = parsear_argumentos()
+
+    data = generar_data(argumentos.cantidad_de_clientes)
+
+    with open(argumentos.archivo_de_salida, "w+") as stream:
+        yaml.dump(data, stream)
+
+
+def parsear_argumentos():
+    parser = argparse.ArgumentParser()
+    parser.add_argument("archivo_de_salida")
+    parser.add_argument("cantidad_de_clientes", type=int)
+
+    return parser.parse_args()
+
+
+main()
