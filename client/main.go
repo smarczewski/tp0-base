3b09760724757fa5e4bb83ef6fbe7831342d5b6e
smarczewski <smarczewski@fi.uba.ar>
2026-03-25 19:05:54 -0300

    Añade handleo de SIGTERM en cliente

--- client/main.go
+++ client/main.go
@@ -3,7 +3,9 @@ package main
 import (
 	"fmt"
 	"os"
+	"os/signal"
 	"strings"
+	"syscall"
 	"time"
 
 	"github.com/op/go-logging"
@@ -111,5 +113,7 @@ func main() {
 	}
 
 	client := common.NewClient(clientConfig)
-	client.StartClientLoop()
+	sigChan := make(chan os.Signal, 1)
+	signal.Notify(sigChan, syscall.SIGTERM)
+	client.StartClientLoop(sigChan)
 }
