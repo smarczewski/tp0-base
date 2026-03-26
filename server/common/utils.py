34cb352b990082f7a69910a224d86006628d40b5
smarczewski <smarczewski@fi.uba.ar>
2026-03-26 11:21:21 -0300

    Añade inicialización de Bet desde una lista con los campos

--- server/common/utils.py
+++ server/common/utils.py
@@ -10,8 +10,18 @@ LOTTERY_WINNER_NUMBER = 7574
 
 
 """ A lottery bet registry. """
+
+
 class Bet:
-    def __init__(self, agency: str, first_name: str, last_name: str, document: str, birthdate: str, number: str):
+    def __init__(
+        self,
+        agency: str,
+        first_name: str,
+        last_name: str,
+        document: str,
+        birthdate: str,
+        number: str,
+    ):
         """
         agency must be passed with integer format.
         birthdate must be passed with format: 'YYYY-MM-DD'.
@@ -24,28 +34,55 @@ class Bet:
         self.birthdate = datetime.date.fromisoformat(birthdate)
         self.number = int(number)
 
+    @classmethod
+    def from_list(cls, field_list):
+        return cls(
+            field_list[0],
+            field_list[1],
+            field_list[2],
+            field_list[3],
+            field_list[4],
+            field_list[5],
+        )
+
+
 """ Checks whether a bet won the prize or not. """
+
+
 def has_won(bet: Bet) -> bool:
     return bet.number == LOTTERY_WINNER_NUMBER
+
 
 """
 Persist the information of each bet in the STORAGE_FILEPATH file.
 Not thread-safe/process-safe.
 """
+
+
 def store_bets(bets: list[Bet]) -> None:
-    with open(STORAGE_FILEPATH, 'a+') as file:
+    with open(STORAGE_FILEPATH, "a+") as file:
         writer = csv.writer(file, quoting=csv.QUOTE_MINIMAL)
         for bet in bets:
-            writer.writerow([bet.agency, bet.first_name, bet.last_name,
-                             bet.document, bet.birthdate, bet.number])
+            writer.writerow(
+                [
+                    bet.agency,
+                    bet.first_name,
+                    bet.last_name,
+                    bet.document,
+                    bet.birthdate,
+                    bet.number,
+                ]
+            )
+
 
 """
 Loads the information all the bets in the STORAGE_FILEPATH file.
 Not thread-safe/process-safe.
 """
+
+
 def load_bets() -> list[Bet]:
-    with open(STORAGE_FILEPATH, 'r') as file:
+    with open(STORAGE_FILEPATH, "r") as file:
         reader = csv.reader(file, quoting=csv.QUOTE_MINIMAL)
         for row in reader:
             yield Bet(row[0], row[1], row[2], row[3], row[4], row[5])
-
